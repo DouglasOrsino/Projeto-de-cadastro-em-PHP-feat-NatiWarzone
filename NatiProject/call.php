24b8b6dba95e8747d3a4d5f6414e0b5a202bfc61
Douglas Orsino <douglas.orsino123@protonmail.com>
2021-03-22 17:28:29 -0300

    Projeto que realizei com a programadora Nati Gamer

---
+++ NatiProject/call.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Abertura de Chamado</title>
+	<meta charset="utf-8">
+</head>
+<body style="text-align: center; background: url('back.jpg'); ">
+<div>
+<h1 style="font-family: sans-serif;">Abertura de Chamados</h1>
+<button><strong><a href="add.php" style="text-decoration: none; color: black">Adicionar Chamado</a></strong></button>
+<button><strong>Histórico de Chamados</strong></button>
+</div>
+</body>
+</html>
