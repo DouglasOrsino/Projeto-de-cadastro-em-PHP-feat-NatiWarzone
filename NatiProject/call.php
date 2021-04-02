7004a158e170d26894eb25049e060e936eafb553
Douglas Orsino <douglas.orsino123@protonmail.com>
2021-04-02 20:57:08 -0300

    Adicionei um carrousel na pagina principal

--- NatiProject/call.php
+++ NatiProject/call.php
@@ -7,6 +7,7 @@
 <head>
 	<title>Abertura de Chamado</title>
 	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body style="text-align: center; background: url('back.jpg'); ">
 <div>
@@ -14,5 +15,37 @@
 <button><strong><a href="add.php" style="text-decoration: none; color: black">Adicionar Chamado</a></strong></button>
 <button><strong>Histórico de Chamados</strong></button>
 </div>
+<div class="carousel" style="padding: 26px 390px;">
+			
+			<div class="carousel__item carousel__item--visible">
+				<img style="width: 496px; height: 250px;" src="img/1.jpg">
+				<p style="width: 496px; height: 250px; line-height: 2em;"><strong>In mathematics and digital electronics, a binary number is a number expressed in the base-2 numeral system or binary numeral system, which uses only two symbols: typically "0" (zero) and "1" (one).
+
+				The base-2 numeral system is a positional notation with a radix of 2. Each digit is referred to as a bit, or binary digit. Because of its straightforward implementation in digital electronic circuitry using logic gates, the binary system is used by almost all modern computers and computer-based devices, as a preferred system of use, over various other human techniques of communication.</strong></p>
+
+			</div>
+			<div class="carousel__item">
+				<img style="width: 496px; height: 250px;" src="img/2.jpg">
+				<p style="width: 496px; height: 250px; line-height: 2em;"><strong>A central processing unit (CPU), also called a central processor, main processor or just processor, is the electronic circuitry that executes instructions comprising a computer program. The CPU performs basic arithmetic, logic, controlling, and input/output (I/O) operations specified by the instructions in the program. This contrasts with external components such as main memory and I/O circuitry,[1] and specialized processors such as graphics processing units (GPUs).
+
+				The form, design, and implementation of CPUs have changed over time, but their fundamental operation remains almost unchanged.</strong></p>
+
+			</div>
+			<div class="carousel__item">
+				<img style="width: 496px; height: 250px;" src="img/3.png">
+				<p style="width: 496px; height: 250px; line-height: 2em;"><strong>Computer security, cybersecurity or information technology security (IT security) is the protection of computer systems and networks from information disclosure, theft of or damage to their hardware, software, or electronic data, as well as from the disruption or misdirection of the services they provide.[1]
+
+				The field is becoming increasingly significant due to the increased reliance on computer systems, the Internet[2] and wireless network standards such as Bluetooth and Wi-Fi, and due to the growth of "smart" devices, including smartphones, televisions, and the various devices that constitute the "Internet of things".</strong></p>
+
+			</div>
+
+			<div class="carousel__actions">
+				<button id="carousel__button--prev" aria-label="previous slide"> < </button>
+				<button id="carousel__button--next" aria-label="next slide"> > </button>
+			</div>
+
+		</div>
+	
+	<script type="text/javascript" src="app.js"></script>
 </body>
 </html>
