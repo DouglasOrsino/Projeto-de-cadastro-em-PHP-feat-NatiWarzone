ca19b839c488ae14653c1848be9056cb596cf2a9
Douglas Orsino <douglas.orsino123@protonmail.com>
2021-04-06 01:53:28 -0300

    Tive que criar uma nova pasta pra que ela rodasse no localhost, excluí o carousel do projeto e substituí por um layout responsivo.

--- ProjectNati/add.php
+++ ProjectNati/add.php
@@ -1,14 +1,17 @@
 <?php
 
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Adicionar Chamado</title>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" type="text/css" href="add.css">
 </head>
-<body style="background: url('back.jpg');">
+<body style="background: url('img/back.jpg');">
 
 <div>	
 	<h1 id="titulo">Cadastrar Chamado</h1>
